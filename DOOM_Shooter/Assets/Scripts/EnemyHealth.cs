785351d3f3d84047a8fb7d8b0624a11115983318
Dudzdude668 <iandudz668@gmail.com>
2025-09-09 12:15:05 -0400

    Messing with Parry mechanics
    Started with parrying mechanics and messed with movement for the speedup.

--- DOOM_Shooter/Assets/Scripts/EnemyHealth.cs
+++ DOOM_Shooter/Assets/Scripts/EnemyHealth.cs
@@ -5,7 +5,8 @@ public class EnemyHealth : MonoBehaviour
     public GameObject enemy;
     public float currentEnemyHealth;
     public float maxEnemyHealth = 10;
-    public float minEnemyHealth = 0;
+    public float minEnemyHealth = 1;
+    public PlayerMovement PlayerMovement;
 
     private void Start()
     {
@@ -14,7 +15,8 @@ public class EnemyHealth : MonoBehaviour
     private void Update()
     {
         if (currentEnemyHealth < minEnemyHealth) 
-        { 
+        {
+            PlayerMovement.SpeedIncrease();
             Destroy(enemy);
         }
     }
