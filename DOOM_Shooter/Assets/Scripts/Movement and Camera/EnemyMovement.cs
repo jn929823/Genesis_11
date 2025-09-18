42a7844cffc26c2f76768e27ca71d8c6dc3bc643
Jreyn17 <jreynolds5756@gmail.com>
2025-09-18 01:42:27 -0400

    Added in Ranged Enemy Prefab
    - Added in assets for Ranged enemy prefab
    - Enemies now rotate toward the player

--- DOOM_Shooter/Assets/Scripts/Movement and Camera/EnemyMovement.cs
+++ DOOM_Shooter/Assets/Scripts/Movement and Camera/EnemyMovement.cs
@@ -4,6 +4,7 @@ public class EnemyMovement : MonoBehaviour
 {
     public GameObject target;
     private Vector3 dirToTarget = Vector3.zero;
+    private Vector3 horizontalDir = Vector3.zero;
     Rigidbody rb;
     private float radius = 5f;
 
@@ -21,21 +22,32 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        dirToTarget = (target.transform.position - transform.position).normalized;
+        //Finds the direction from the transform to the target
+        dirToTarget = (target.transform.position - transform.position);
+        //Sets the y direction to 0
+        dirToTarget.y = 0;
+        //Normalize the dirToTarget to get the horizontal direction w/o the y direction
+        horizontalDir = dirToTarget.normalized;
     }
 
     void FixedUpdate()
     {
+        //Finds the player's position, uses its own y position so it doesn't travel upward
         Vector3 targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        //Move the enemy toward the player
         transform.position = Vector3.MoveTowards(this.transform.position, targetPos, 3 * Time.deltaTime);
+        //Rotate the enemy toward the player
+        transform.rotation = Quaternion.LookRotation(horizontalDir, Vector3.up);
+
+        //Still need to check for if in range for ranged enemies
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + dirToTarget * 2);
+        Gizmos.DrawLine(transform.position, transform.position + horizontalDir * 2);
 
-        Gizmos.color = Color.yellow;
+        //Gizmos.color = Color.yellow;
         //Gizmos.DrawSphere(transform.position, radius);
     }
 }
