81e2e8d575b3d2b960785fa732b4c5bcdd099a7a
Jreyn17 <jreynolds5756@gmail.com>
2025-10-13 21:51:37 -0400

    jump actually works

--- DOOM_Shooter/Assets/Scripts/Movement and Camera/Jump.cs
+++ DOOM_Shooter/Assets/Scripts/Movement and Camera/Jump.cs
@@ -4,7 +4,7 @@ public class Jump : MonoBehaviour
 {
     public float jumpHeight;
     private Rigidbody rb;
-    public bool canJump = true;
+    public bool isGrounded = true;
     public AudioSource jumpAudio;
 
     private void Start()
@@ -16,29 +16,32 @@ public class Jump : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (canJump == true /*&& IsGrounded()*/)  //Uncomment when IsGrounded works properly
+            if (isGrounded)
             {
                 PlayerJump();
                 if (jumpAudio != null) jumpAudio.Play();
-
             }
         }
     }
 
     void PlayerJump()
     {
-        canJump = false;
         rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-        Invoke("ResetJump", 1f);
     }
 
-    void ResetJump()
+    void OnCollisionEnter(Collision collision)
     {
-        canJump = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 
-    /*bool IsGrounded()
+    void OnCollisionExit(Collision collision)
     {
-        //Needs some kind of raycast check
-    }*/
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
